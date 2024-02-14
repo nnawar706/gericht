da22f82174aec15575645cf77840aebc4ea3a895
nnawar706 <nnawar706@gmail.com>
2024-02-14 15:46:49 +0600

    header complete

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,16 @@
 import Button from "./ui/Button"
-import {welcome} from "../assets"
+import {spoon, welcome} from "../assets"
 
 const Hero = () => {
     return (
         <section id="home" className="flex flex-col md:flex-row justify-between items-center
         min-h-[100vh] bg-black-primary py-16 px-24">
             <div className="flex flex-1 flex-col w-full justify-center items-start">
+                <div className="mb-4">
+                    <p className="font-base text-white font-bold tracking-wider leading-7 text-2xl uppercase mb-1"
+                       style={{ fontFeatureSettings: "'tnum' on, 'lnum' on" }}>chase the new flavor</p>
+                    <img src={spoon} alt="spoon" width="45px"/>
+                </div>
                 <h1 className="font-base text-golden-primary font-bold tracking-wider uppercase text-6xl">
                     the key to fine dining
                 </h1>
